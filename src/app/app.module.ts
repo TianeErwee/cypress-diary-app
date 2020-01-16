9f76c6156f820a24681708b2126ce94b6a899e84
Tiane Erwee <tiane.erwee@gmail.com>
2020-01-16 12:05:51 +0200

    moved modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DiaryEntriesModule } from './diary-entries/diary-entries.module';
+import { DiaryEntriesModule } from './modules/diary-entries/diary-entries.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { DiaryEntriesModule } from './diary-entries/diary-entries.module';
     BrowserModule,
     DiaryEntriesModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
