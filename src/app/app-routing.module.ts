f60bd165682e9d7545e1ba79a830f393933c268c
Tiane Erwee <tiane.erwee@gmail.com>
2020-01-17 14:12:59 +0200

    added ids

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
